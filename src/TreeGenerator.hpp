22b05fd68fe51d61cdf6e7080438b67a8dfeb08b
Pedro Pereira <pedrovbpereira10@gmail.com>
2025-10-12 22:51:28 -0300

    Feat: generateTree function

--- src/TreeGenerator.hpp
+++ src/TreeGenerator.hpp
@@ -1,21 +1,21 @@
 #include <queue>
 #include <string>
-#include <utility>
 #include <vector>
 
 class Node{
     public:
+    Node* father = nullptr;
     Node* left = nullptr;
     Node* right = nullptr;
-    Node* father = nullptr;
-    std::pair<std::string, size_t> pair;
+
     std::string symbol;
     size_t value;
 
+    Node() {};
     Node(Node* n1, Node* n2, Node* n3, std::string s, size_t f){
+        this->father = n3;
         this->left = n1;
         this->right = n2;
-        this->father = n3;
         this->symbol = s;
         this->value = f;
     }
@@ -23,6 +23,7 @@ class Node{
 
 class Tree{
     public: 
+    Node* main_node = nullptr;
 
     struct CompareByKey {
         bool operator()(const Node* a, const Node* b) const {
@@ -32,5 +33,5 @@ class Tree{
 
     std::priority_queue<Node*, std::vector<Node*>, CompareByKey> pq;
 
-    void generateTree(std::priority_queue<Node*, std::vector<Node*>, CompareByKey>, Node*);
+    Node* generateTree(std::priority_queue<Node*, std::vector<Node*>, CompareByKey>);
 };
